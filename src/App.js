6797581d4d30054425d3e9021f06ca053ab4bab1
naimatullahofficial <support@naimatullah.com>
2026-03-06 00:46:12 +0500

    memoization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Calculator from "./Calculator";
 import ToggleSounds from "./ToggleSounds";
 
@@ -9,7 +9,8 @@ function App() {
   // Will be be AM or PM
   const partOfDay = time.slice(-2);
 
-  const workouts = [
+  const workouts = useMemo(()=> {
+    return [
     {
       name: "Full-body workout",
       numExercises: partOfDay === "AM" ? 9 : 8,
@@ -30,7 +31,8 @@ function App() {
       name: "Core only",
       numExercises: partOfDay === "AM" ? 5 : 4,
     },
-  ];
+  ]
+  }, [partOfDay]);
 
   function formatTime(date) {
     return new Intl.DateTimeFormat("en", {
@@ -53,7 +55,7 @@ function App() {
   return (
     <main>
       <h1>Workout timer</h1>
-      <time>For your workout on {time}</time>
+      <time>For your workout on {time}</time>/
       <ToggleSounds allowSound={allowSound} setAllowSound={setAllowSound} />
       <Calculator workouts={workouts} allowSound={allowSound} />
     </main>
